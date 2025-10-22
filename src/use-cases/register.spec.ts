6d1ecba9d39f7089b0e0062d6e676fe46c756ba3
Igor Ferreira <ferreiraigor007@gmail.com>
2025-10-21 22:02:04 -0300

    In-Memory Databases

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,12 +1,26 @@
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository.ts"
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository.ts"
+import { UserAlreadyExistsError } from "./erros/user-already-exists-error.ts"
 import { RegisterUseCase } from "./register.ts"
 import { describe, expect, it } from "vitest"
 import { compare } from "bcryptjs"
 
 describe("Register Use Case", () => {
+  it("should be able to register", async () => {
+    const sud = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(sud)
+
+    const { user } = await registerUseCase.execute({
+      name: "Igor Ferreira",
+      email: "igor@email.com",
+      password: "123456",
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
   it("should hash user password upon registration", async () => {
-    const prismaUsersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+    const sud = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(sud)
 
     const { user } = await registerUseCase.execute({
       name: "Igor Ferreira",
@@ -17,5 +31,26 @@ describe("Register Use Case", () => {
     const isPasswordCorrectlyHashed = await compare("123456", user.passwordHash)
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it("should not be able to register with same email twice", async () => {
+    const sud = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(sud)
+
+    const email = "igor@email.com"
+
+    await registerUseCase.execute({
+      name: "Igor Ferreira",
+      email,
+      password: "123456",
+    })
+
+    expect(async () => {
+      await registerUseCase.execute({
+        name: "Igor Ferreira",
+        email,
+        password: "123456",
+      })
+    }).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
