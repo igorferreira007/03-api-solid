8b8ba49b74301ed44efe670c7134861730e1e95e
Igor Ferreira <ferreiraigor007@gmail.com>
2025-10-24 18:48:07 -0300

    Caso de uso de check-in

---
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -0,0 +1,21 @@
+import { CheckInsRepository } from "../check-ins-repository.ts"
+import { CheckIn, Prisma } from "@prisma/client"
+import { randomUUID } from "node:crypto"
+
+export class InMemoryCheckInsRepository implements CheckInsRepository {
+  public items: CheckIn[] = []
+
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn: CheckIn = {
+      id: randomUUID(),
+      userId: data.userId,
+      gymId: data.gymId,
+      validatedAt: data.validatedAt ? new Date(data.validatedAt) : null,
+      createdAt: new Date(),
+    }
+
+    this.items.push(checkIn)
+
+    return checkIn
+  }
+}
