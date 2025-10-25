f2ff58cb96a67a603b56ee8cb29e5d61240845e4
Igor Ferreira <ferreiraigor007@gmail.com>
2025-10-25 18:00:06 -0300

    Validando distância do check-in

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -19,8 +19,8 @@ describe("Check-in Use Case", () => {
       title: "JS Gym",
       description: "",
       phone: "",
-      latitude: new Decimal(0),
-      longitude: new Decimal(0),
+      latitude: new Decimal(-23.21853715183315),
+      longitude: new Decimal(-45.808786372274426),
     })
 
     vi.useFakeTimers()
@@ -34,8 +34,8 @@ describe("Check-in Use Case", () => {
     const { checkIn } = await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
-      userLatitude: -23.2092929,
-      userLongitude: -45.7995071,
+      userLatitude: -23.21853715183315,
+      userLongitude: -45.808786372274426,
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
@@ -47,16 +47,16 @@ describe("Check-in Use Case", () => {
     await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
-      userLatitude: -23.2092929,
-      userLongitude: -45.7995071,
+      userLatitude: -23.21853715183315,
+      userLongitude: -45.808786372274426,
     })
 
     await expect(() =>
       sut.execute({
         gymId: "gym-01",
         userId: "user-01",
-        userLatitude: -23.2092929,
-        userLongitude: -45.7995071,
+        userLatitude: -23.21853715183315,
+        userLongitude: -45.808786372274426,
       })
     ).rejects.toBeInstanceOf(Error)
   })
@@ -67,8 +67,8 @@ describe("Check-in Use Case", () => {
     await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
-      userLatitude: -23.2092929,
-      userLongitude: -45.7995071,
+      userLatitude: -23.21853715183315,
+      userLongitude: -45.808786372274426,
     })
 
     vi.setSystemTime(new Date(2025, 8, 21, 8, 0, 0))
@@ -76,10 +76,30 @@ describe("Check-in Use Case", () => {
     const { checkIn } = await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
-      userLatitude: -23.2092929,
-      userLongitude: -45.7995071,
+      userLatitude: -23.21853715183315,
+      userLongitude: -45.808786372274426,
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it("should not be able to check in on distant gym", async () => {
+    gymsRepository.items.push({
+      id: "gym-02",
+      title: "JS Gym",
+      description: "",
+      phone: "",
+      latitude: new Decimal(-23.199689017999738),
+      longitude: new Decimal(-45.88156857907786),
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: "gym-02",
+        userId: "user-01",
+        userLatitude: -23.21853715183315,
+        userLongitude: -45.808786372274426,
+      })
+    ).rejects.toBeInstanceOf(Error)
   })
 })
--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -2,6 +2,7 @@ import { CheckIn } from "@prisma/client"
 import { CheckInsRepository } from "@/repositories/check-ins-repository.ts"
 import { GymsRepository } from "@/repositories/gyms-repository.ts"
 import { ResourceNotFoundError } from "./erros/resource-not-found-error.ts"
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates.ts"
 
 interface CheckInUseCaseRequest {
   userId: string
@@ -23,6 +24,8 @@ export class CheckInUseCase {
   async execute({
     userId,
     gymId,
+    userLatitude,
+    userLongitude,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
     const gym = await this.gymsRepository.findById(gymId)
 
@@ -30,7 +33,16 @@ export class CheckInUseCase {
       throw new ResourceNotFoundError()
     }
 
-    // Calcular a distancia entre o usuário e a academia
+    const distance = getDistanceBetweenCoordinates(
+      { latitude: userLatitude, longitude: userLongitude },
+      { latitude: gym.latitude.toNumber(), longitude: gym.longitude.toNumber() }
+    )
+
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1
+
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) {
+      throw new Error()
+    }
 
     const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
       userId,
