918209356378d4d5931ff7b592aeecfd16506a9a
Igor Ferreira <ferreiraigor007@gmail.com>
2025-11-02 19:17:54 -0300

    Controllers das rotas de check-ins

---
+++ src/http/controllers/check-ins/routes.ts
@@ -0,0 +1,16 @@
+import { FastifyInstance } from "fastify"
+import { verifyJWT } from "../../middlewares/verify-jwt.ts"
+import { create } from "./create.ts"
+import { validate } from "./validate.ts"
+import { history } from "./history.ts"
+import { metrics } from "./metrics.ts"
+
+export async function checkInsRoutes(app: FastifyInstance) {
+  app.addHook("onRequest", verifyJWT)
+
+  app.get("/check-ins/history", history)
+  app.get("/check-ins/metrics", metrics)
+
+  app.post("/gyms/:gymId/check-ins", create)
+  app.patch("/check-ins/:checkInId/validate", validate)
+}
