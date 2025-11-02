881cdaaee47ae970633405170642896b48367dc0
Igor Ferreira <ferreiraigor007@gmail.com>
2025-11-02 18:37:02 -0300

    Outros controllers da academia

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,14 @@
 import { FastifyInstance } from "fastify"
 import { verifyJWT } from "../../middlewares/verify-jwt.ts"
+import { search } from "./search.ts"
+import { nearby } from "./nearby.ts"
+import { create } from "./create.ts"
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT)
+
+  app.get("/gyms/search", search)
+  app.get("/gyms/nearby", nearby)
+
+  app.post("/gyms/create", create)
 }
