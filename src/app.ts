7a10f48429a67adbcc7db67d5ec8c8b5862eafb2
Igor Ferreira <ferreiraigor007@gmail.com>
2025-11-06 19:28:39 -0300

    Estratégia de refresh de token

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { usersRoutes } from "./http/controllers/users/routes.ts"
 import fastify from "fastify"
 import { env } from "./env/index.ts"
 import fastifyJwt from "@fastify/jwt"
+import fastifyCookie from "@fastify/cookie"
 import { gymsRoutes } from "./http/controllers/gyms/routes.ts"
 import { checkInsRoutes } from "./http/controllers/check-ins/routes.ts"
 
@@ -10,7 +11,16 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
