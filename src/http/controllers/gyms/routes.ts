9e5ea24ecb9a57481b90b51681438178a8d108c7
Igor Ferreira <ferreiraigor007@gmail.com>
2025-11-08 11:44:50 -0300

    Autorização por cargos (RBAC)

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,6 +3,7 @@ import { verifyJWT } from "../../middlewares/verify-jwt.ts"
 import { search } from "./search.ts"
 import { nearby } from "./nearby.ts"
 import { create } from "./create.ts"
+import { verifyUserRole } from "@/http/middlewares/verify-user-role.ts"
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT)
@@ -10,5 +11,5 @@ export async function gymsRoutes(app: FastifyInstance) {
   app.get("/gyms/search", search)
   app.get("/gyms/nearby", nearby)
 
-  app.post("/gyms/create", create)
+  app.post("/gyms/create", { onRequest: [verifyUserRole("ADMIN")] }, create)
 }
